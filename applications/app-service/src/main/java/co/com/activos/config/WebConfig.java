f4f5e24f31f6354ac63f0f51d30794c7e9655e95
Deiby Sierra <deiby.sierra@sofka.com.co>
2025-09-16 15:33:13 -0500

    ajuste jar

---
+++ applications/app-service/src/main/java/co/com/activos/config/WebConfig.java
@@ -0,0 +1,25 @@
+package co.com.activos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // Usa allowedOriginPatterns para soportar patrones y evitar problemas con credenciales
+                .allowedOriginPatterns(
+                        "http://localhost:4200",
+                        "https://front-gestor-document-7r7hrmn7hq-ue.a.run.app",
+                        "https://front-gestor-document-194964492367.us-east1.run.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Content-Disposition")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
